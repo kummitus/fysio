8578345f79efb054e1d66119b21045f5f13c30fd
Tapio Ikkala <tapio.ikkala@relex.fi>
2017-05-25 15:20:43 +0300

    Added layers and categories table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170517100012) do
+ActiveRecord::Schema.define(version: 20170525121621) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "layer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "layers", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
