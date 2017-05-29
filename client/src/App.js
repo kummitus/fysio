dba0bb2e177456d1a5191c37b0af3774d377265c
Tapio Ikkala <tapio.ikkala@relex.fi>
2017-05-29 17:17:15 +0300

    tapio-frontend_sketching

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container,
+    Row,
+    Col
+} from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Header />
+        <TopLevelGrid />
       </div>
     );
   }
+}
+
+class Header extends React.Component {
+    render(){
+        return (
+            <Navbar color="faded" light toggleable>
+                <NavbarToggler/>
+                <NavbarBrand href="#">physiorepo</NavbarBrand>
+                <Collapse navbar>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <NavLink href="#">Home</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="#">Login</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="#">Register</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        )
+    }
+}
+
+class TopLevelGrid extends React.Component {
+    render(){
+        return (
+        <Container>
+            <Row>
+                <Col>Layer</Col>
+                <Col>Publication</Col>
+            </Row>
+        </Container>
+        )
+    }
 }
 
 export default App;
